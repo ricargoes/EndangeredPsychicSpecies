a8e966557b1a0e84a782b22c86d3f5217c5920d6
Ricardo <ricardogomezescalante@gmail.com>
2020-04-18 19:44:07 +0200

    carrier animation stuff

---
+++ actors/Carrier.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+var carrying = true
+var max_speed = 150.0
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	if Input.is_action_just_pressed("carrier_dash"):
+		try_dashing()
+	if Input.is_action_just_pressed("carrier_grab") and not carrying:
+		grab()
+	
+	var movement_dir = Vector2.ZERO
+	if Input.is_action_pressed("carrier_move_up"):
+		movement_dir += Vector2.UP
+	if Input.is_action_pressed("carrier_move_down"):
+		movement_dir += Vector2.DOWN
+	if Input.is_action_pressed("carrier_move_right"):
+		movement_dir += Vector2.RIGHT
+	if Input.is_action_pressed("carrier_move_left"):
+		movement_dir += Vector2.LEFT
+	
+	var speed = max_speed*3/4.0*movement_dir.normalized()
+	if carrying:
+		speed *= 3/4.0
+	
+	set_rotation(movement_dir.angle())
+	move_and_slide(speed)
+	if speed == Vector2.ZERO:
+		$Sprite.play("stand")
+	else:
+		$Sprite.play("running")
+
+func try_dashing():
+	pass
+
+func grab():
+	pass
