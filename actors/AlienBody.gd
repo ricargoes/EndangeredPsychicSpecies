66b83c642e0459e5e262e307fb6c17685c7c69a0
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-04-19 19:06:27 +0200

    Separate scenery from level

--- actors/AlienBody.gd
+++ actors/AlienBody.gd
@@ -10,7 +10,7 @@ func _process(delta):
 	if Input.is_action_pressed("alien_set_destination"):
 		destination = get_global_mouse_position()
 		var pos = get_position()
-		var points_to_destination = get_parent().get_node("Navigation2D").get_simple_path(get_position(), destination)
+		var points_to_destination = get_parent().get_node("Background/Navigation2D").get_simple_path(get_position(), destination)
 		get_parent().get_node("Line2D").points = points_to_destination
 		var vector_to_next = points_to_destination[1] - get_position()
 		if points_to_destination[-1] != points_to_destination[1] or vector_to_next.length() > 20:
