71413f0e202aa5b072f126f0ef5e065ffb3802a2
Ricardo <ricardogomezescalante@gmail.com>
2020-04-19 23:47:02 +0200

    Garbage containers can spawn bottles and bags

--- throwables/ThrowableObject.gd
+++ throwables/ThrowableObject.gd
@@ -12,11 +12,8 @@ func _on_ImpulseTime_timeout():
 func _on_ThrowableObject_body_entered(body):
 	if body.has_method("hit"):
 		body.hit()
-	if breakable:
-		State.play_sound($Hit.stream)
-		queue_free()
-	else:
-		$Hit.play(0.25)
+	
+	own_damage()
 
 func _on_ThrowableObject_sleeping_state_changed():
 	if not sleeping:
@@ -26,3 +23,10 @@ func _on_ThrowableObject_sleeping_state_changed():
 func _on_SelectableArea_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("alien_grab_object"):
 		get_tree().call_group("alien_mind", "grab_object", self)
+
+func own_damage():
+	if breakable:
+		State.play_sound($Hit.stream)
+		queue_free()
+	else:
+		$Hit.play(0.25)
