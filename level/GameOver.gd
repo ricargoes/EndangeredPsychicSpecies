f7671e7761f54123e10f7499693c1e955f5adc91
Ricardo <ricardogomezescalante@gmail.com>
2020-04-19 16:26:15 +0200

    first version of GameOver screen

---
+++ level/GameOver.gd
@@ -0,0 +1,25 @@
+extends Control
+
+func _ready():
+	if State.alien_wins:
+		_alien_wins()
+	else:
+		_carrier_wins()
+	
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+	elif event.is_action_type():
+		get_tree().change_scene("res://level/Level.tscn")
+
+func _alien_wins():
+	$VBoxContainer/Who.text = "Alien wins"
+	$VBoxContainer/HBoxContainer/Descrption.text = "The baby alien gets killed while exploring the world. Well, at least he died FREE!"
+	$VBoxContainer/HBoxContainer/Picture.texture = preload("res://actors/sprites/creaturecrawling2.png")
+
+func _carrier_wins():
+	$VBoxContainer/Who.text = "Frank wins"
+	$VBoxContainer/HBoxContainer/Descrption.text = "Frank manages to keep the alien alive desite its best efforts. The alien is not mature enough to know the world is dangerous. Frank knows best."
+	$VBoxContainer/HBoxContainer/Picture.texture = preload("res://actors/sprites/daddystanding_reloj4.png")
