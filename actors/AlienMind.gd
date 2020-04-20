4bfbcdacd4bba62f03322db8037055678c5e1e84
Ricardo <ricardogomezescalante@gmail.com>
2020-04-20 23:17:33 +0200

    ESC exits the game

--- actors/AlienMind.gd
+++ actors/AlienMind.gd
@@ -9,6 +9,9 @@ func _ready():
 	set_process_input(true)
 
 func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+	
 	if event.is_action_released("alien_grab_object") and _throwing_object:
 		var throwing_vector = get_global_mouse_position() - _throwing_object.get_global_position()
 		_throwing_object.impulse(telekinesis_strength*throwing_vector)
