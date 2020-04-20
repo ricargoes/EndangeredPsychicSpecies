72747bf6bed8d66354b6ec31da4e580598219d9b
Ricardo <ricardogomezescalante@gmail.com>
2020-04-20 14:00:03 +0200

    A way to start with the procgen level

--- level/Welcome.gd
+++ level/Welcome.gd
@@ -7,4 +7,8 @@ func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 	elif event.is_action_pressed("ui_accept"):
-		get_tree().change_scene("res://level/Level.tscn")
+		State.city = "res://level/Level.tscn"
+		get_tree().change_scene(State.city)
+	elif event.is_action_pressed("ui_accept_other"):
+		State.city = "res://level/ProcGenLevel.tscn"
+		get_tree().change_scene(State.city)
