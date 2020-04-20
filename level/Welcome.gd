bfc9aec3c42385d91fbb18521fe9bfd89cdb3d67
Ricardo <ricardogomezescalante@gmail.com>
2020-04-20 14:25:46 +0200

    Typo calling level

--- level/Welcome.gd
+++ level/Welcome.gd
@@ -7,4 +7,4 @@ func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 	elif event.is_action_pressed("ui_accept"):
-		get_tree().change_scene("res://level/ProGenLevel.tscn")
+		get_tree().change_scene("res://level/ProcGenLevel.tscn")
