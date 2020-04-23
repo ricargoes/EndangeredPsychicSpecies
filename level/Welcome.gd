2efe2b94e451a07d25b1b533e5a01a811b22fcc1
Ricardo <ricardogomezescalante@gmail.com>
2020-04-23 18:13:44 +0200

    Some redestribution of welcome screen

--- level/Welcome.gd
+++ level/Welcome.gd
@@ -26,3 +26,8 @@ func _on_Large_pressed():
 
 func _on_Quit_pressed():
 	get_tree().quit()
+
+
+func _on_ShowControls_pressed():
+	$AlienControls.visible = not $AlienControls.visible
+	$RichardControls.visible = not $RichardControls.visible
