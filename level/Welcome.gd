353b086aed30783bd9448f18c25caf0bf780363c
Ricardo <ricardogomezescalante@gmail.com>
2020-04-23 10:23:55 +0200

    Added three modes

--- level/Welcome.gd
+++ level/Welcome.gd
@@ -6,5 +6,23 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
-	elif event.is_action_pressed("ui_accept"):
-		get_tree().change_scene("res://level/ProcGenLevel.tscn")
+
+
+func _on_Tutorial_pressed():
+	pass # Replace with function body.
+
+
+func _on_Rush_pressed():
+	get_tree().change_scene("res://level/LevelRush.tscn")
+
+
+func _on_Standard_pressed():
+	get_tree().change_scene("res://level/LevelStandard.tscn")
+
+
+func _on_Large_pressed():
+	get_tree().change_scene("res://level/LevelLarge.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
